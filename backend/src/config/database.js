680c495a170440cd4d1e977c4d6f98ff2fe7188d
AlvaroAl6 <20192352@aloe.ulima.edu.pe>
2026-06-01 02:28:34 -0500

    Forzar IPv4

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  family: 4,
 });
 
 pool.on('error', (err) => {
